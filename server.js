3cbf2587c4268d277a89261b5e3a12dbe8ba5bfc
PopolQue <davidcutura@icloud.com>
2024-08-18 14:16:38 +0200

    MongoConnected

--- server.js
+++ server.js
@@ -6,9 +6,15 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect('your-mongo-atlas-connection-string', { useNewUrlParser: true, useUnifiedTopology: true });
-
-app.get('/', (req, res) => res.send('Kanban API'));
-
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const mongoURI = process.env.MONGO_URI;
+
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.error(err));
+
+app.get('/', (req, res) => res.send('Hello from Kanban Backend!'));
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
